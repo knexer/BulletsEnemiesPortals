04190bc58753eca080ebe6e71507ba4c7e747dce
knexer <knexer@gmail.com>
2014-08-23 14:22:54 -0700

    Add collision scripts

---
+++ BulletsEnemiesPortals/Assets/DestroyOtherOnCollision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOtherOnCollision : MonoBehaviour {
+
+    public LayerMask LayerMask;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if ((LayerMask & 1 << other.gameObject.layer) > 0)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
