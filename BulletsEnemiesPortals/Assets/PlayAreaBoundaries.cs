177e72595c46b2c276c21fb16b00d3bc1605e35f
knexer <knexer@gmail.com>
2014-08-23 16:44:52 -0700

    Define the play area with this one weird trick

---
+++ BulletsEnemiesPortals/Assets/PlayAreaBoundaries.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAreaBoundaries : MonoBehaviour {
+
+    public Rect UpperWorld;
+    public Rect LowerWorld;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
