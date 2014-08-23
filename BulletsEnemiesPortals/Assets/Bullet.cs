67a3a0ceca15aa3258ee1dac6ba2fd70a9ed33d1
Marjie Volk <marjie.volk@gmail.com>
2014-08-23 16:12:23 -0700

    Added bullet prefabs, and option to give bullets spin

--- BulletsEnemiesPortals/Assets/Bullet.cs
+++ BulletsEnemiesPortals/Assets/Bullet.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
+    public float startingAngularVelocity = 0;
+
 	// Use this for initialization
 	void Start () {
-	
+        rigidbody2D.angularVelocity = startingAngularVelocity;
 	}
 	
 	// Update is called once per frame
