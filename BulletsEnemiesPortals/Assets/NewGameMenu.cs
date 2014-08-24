d6f70200c38d45b74427c3409fdd75765054bf02
Marjie Volk <marjie.volk@gmail.com>
2014-08-23 21:58:28 -0700

    New GUISkin

---
+++ BulletsEnemiesPortals/Assets/NewGameMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGameMenu : MonoBehaviour {
+
+    public GUISkin skin;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI() {
+        GUI.skin = skin;
+        GUI.Window(0, new Rect(200, 50, 700, 700), layoutWindow, "Bullets, Enemies,\nPortals");
+    }
+
+    public void layoutWindow(int id) {
+        GUILayout.Space(100);
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.Button("Start");
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+    }
+}
