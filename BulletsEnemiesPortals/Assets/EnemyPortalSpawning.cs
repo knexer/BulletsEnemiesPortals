60a35b597e04fc41decc4bc41b687f94b2c6788e
Marjie <marjie.volk@gmail.com>
2014-08-23 17:15:04 -0700

    Misc additions to enemy code.

--- BulletsEnemiesPortals/Assets/EnemyPortalSpawning.cs
+++ BulletsEnemiesPortals/Assets/EnemyPortalSpawning.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class EnemyPortalSpawning : MonoBehaviour {
-
+    public float lastSpawn;
+    public float spawnRate;
 	// Use this for initialization
 	void Start () {
-	
+        lastSpawn = Time.time;
+        spawnRate = 2.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Time.time - lastSpawn > spawnRate) {
+            spawnEnemy();
+            lastSpawn = Time.time;
+        }
 	}
+
+    private void spawnEnemy() {
+        GameObject enemyInst = (GameObject) Instantiate(enemy.gameObject, transform.position, enemy.gameObject.transform.rotation);
+    }
 }
