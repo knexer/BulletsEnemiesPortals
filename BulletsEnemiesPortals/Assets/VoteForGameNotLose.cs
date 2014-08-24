e569a74c43007075f9dc929d8408d71a36c217cf
knexer <knexer@gmail.com>
2014-08-23 22:30:59 -0700

    Vote for Game not Loose

---
+++ BulletsEnemiesPortals/Assets/VoteForGameNotLose.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoteForGameNotLose : MonoBehaviour {
+
+    static int numVotes = 0;
+
+	// Use this for initialization
+	void Start () {
+        numVotes++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDestroy()
+    {
+        numVotes--;
+        if (numVotes <= 0)
+        {
+            Debug.Log("YOU ARE NOW LOSE");
+        }
+    }
+}
