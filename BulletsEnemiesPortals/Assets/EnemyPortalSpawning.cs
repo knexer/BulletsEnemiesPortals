2cea388379dc55dac5e0b010ff4bc6f10da879f7
Marjie <marjie.volk@gmail.com>
2014-08-23 18:36:39 -0700

    Stuff.

--- BulletsEnemiesPortals/Assets/EnemyPortalSpawning.cs
+++ BulletsEnemiesPortals/Assets/EnemyPortalSpawning.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class EnemyPortalSpawning : MonoBehaviour {
     public float lastSpawn;
     public float spawnRate;
-    public GameObject spawnee;
+    public Enemy spawnee;
 	// Use this for initialization
 	void Start () {
         lastSpawn = Time.time;
