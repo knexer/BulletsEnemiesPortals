0f858ef58cc88f7f2482e0d4e82e924aa538e65a
Marjie Volk <marjie.volk@gmail.com>
2014-08-24 10:34:25 -0700

    Small bug in enemy portal spawning - spawn rate set in editor is now respected

--- BulletsEnemiesPortals/Assets/Scripts/EnemyPortalSpawning.cs
+++ BulletsEnemiesPortals/Assets/Scripts/EnemyPortalSpawning.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class EnemyPortalSpawning : MonoBehaviour {
     private float lastSpawn;
-    public float spawnRate;
+    public float spawnRate = 2.0f;
     public Enemy spawnee;
 	// Use this for initialization
 	void Start () {
         lastSpawn = Time.time;
-        spawnRate = 2.0f;
 	}
 	
 	// Update is called once per frame
