b4da28af9da2e61d74a848e90a0d36ad6de7648a
Marjie Volk <marjie.volk@gmail.com>
2014-08-23 18:03:16 -0700

    Added some customization to the shooter script

--- BulletsEnemiesPortals/Assets/Shooter.cs
+++ BulletsEnemiesPortals/Assets/Shooter.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Shooter : MonoBehaviour {
 
     public Bullet bullet;
+    public float startingVelocity = 6;
+    public Direction startingDirection = Direction.LEFT;
 
     private float lastBulletTime = 0;
 
@@ -21,7 +23,12 @@ public class Shooter : MonoBehaviour {
 	}
 
     private void spawnBullet() {
-        GameObject bulletInst = (GameObject) Instantiate(bullet.gameObject, transform.position, bullet.gameObject.transform.rotation);
-        bulletInst.rigidbody2D.velocity = new Vector2(-6, 0);
+        bool isLeft = startingDirection == Direction.LEFT;
+        GameObject bulletInst = (GameObject) Instantiate(bullet.gameObject, transform.position, Quaternion.Euler(0, 0, (isLeft ? 1 : -1) * 90));
+        bulletInst.rigidbody2D.velocity = new Vector2((isLeft ? -1 : 1) * startingVelocity, 0);
+    }
+
+    public enum Direction {
+        LEFT, RIGHT
     }
 }
