5250d784e7f2c888cc3835d5b979daa9a0319090
Marjie Volk <marjie.volk@gmail.com>
2014-08-24 18:36:05 -0700

    Updated enemy portal prefab

--- BulletsEnemiesPortals/Assets/Scripts/Menus/MainMenu.cs
+++ BulletsEnemiesPortals/Assets/Scripts/Menus/MainMenu.cs
@@ -1,54 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class MainMenu : MonoBehaviour {
 
     public GUISkin skin;
 
+    private GUIStyle largeButton;
+    private Menu[] menus;
+    private int currentMenu = 0;
+
     private const int size = 570;
 
     void Start() {
         Time.timeScale = 1;
+        largeButton = new GUIStyle(skin.button);
+        largeButton.fontSize = 50;
+        
+        menus = new Menu[] {
+            new Menu("Bullets, Enemies,\nPortals", mainMenu),
+            new Menu("Unlocks", unlocksMenu),
+            new Menu("Options", optionsMenu)
+        };
     }
 
     void OnGUI() {
         GUI.skin = skin;
-        GUI.Window(0, new Rect((Screen.width - size) / 2, (Screen.height - size) / 2, size, size), layoutWindow, "Bullets, Enemies,\nPortals");
+        GUI.Window(0, new Rect((Screen.width - size) / 2, (Screen.height - size) / 2, size, size), menus[currentMenu].function, menus[currentMenu].title);
     }
 
-    public void layoutWindow(int id) {
+    public void mainMenu(int id) {
         GUILayout.FlexibleSpace();
 
-        GUIStyle largeButton = new GUIStyle(skin.button);
-        largeButton.fontSize = 50;
-        GUILayout.BeginHorizontal();
-        GUILayout.Space(100);
-        if (GUILayout.Button("Play", largeButton)) {
-            Application.LoadLevel("SkittlesWorld");
-        }
-        GUILayout.Space(100);
-        GUILayout.EndHorizontal();
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Space(100);
-        GUILayout.Button("Unlocks");
-        GUILayout.Space(100);
-        GUILayout.EndHorizontal();
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Space(100);
-        GUILayout.Button("Options");
-        GUILayout.Space(100);
-        GUILayout.EndHorizontal();
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Space(100);
-        if (GUILayout.Button("Exit")) {
-            Application.Quit();
-        }
-        GUILayout.Space(100);
-        GUILayout.EndHorizontal();
+        printButton("Play!", () => Application.LoadLevel("SkittlesWorld"), largeButton);
+        printButton("Unlocks", () => currentMenu = 1);
+        printButton("Options", () => currentMenu = 2);
+        printButton("Exit", () => Application.Quit());
 
         GUILayout.Space(20);
+    }
+
+    public void unlocksMenu(int id) {
+        GUILayout.FlexibleSpace();
+        printButton("Return to Main Menu", () => currentMenu = 0);
+    }
+
+    public void optionsMenu(int id) {
+        GUILayout.FlexibleSpace();
+        printButton("Return to Main Menu", () => currentMenu = 0);
+    }
+
+
+
+    private void printButton(string text, Action onClick, GUIStyle style = null) {
+        GUILayout.BeginHorizontal();
+        GUILayout.Space(100);
+        if (style == null) style = skin.button;
+        if (GUILayout.Button(text, style)) {
+            onClick();
+        }
+        GUILayout.Space(100);
+        GUILayout.EndHorizontal();
+    }
+
+    private class Menu {
+        public string title;
+        public GUI.WindowFunction function;
+
+        public Menu(string title, GUI.WindowFunction function) {
+            this.title = title;
+            this.function = function;
+        }
     }
 }
