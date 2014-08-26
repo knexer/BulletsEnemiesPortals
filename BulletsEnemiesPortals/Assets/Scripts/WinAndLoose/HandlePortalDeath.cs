193d50bc5e02cf4803344aa49fdc86b6442ae932
knexer <knexer@gmail.com>
2014-08-25 17:11:01 -0700

    fix winning with one enemy portal left

--- BulletsEnemiesPortals/Assets/Scripts/WinAndLoose/HandlePortalDeath.cs
+++ BulletsEnemiesPortals/Assets/Scripts/WinAndLoose/HandlePortalDeath.cs
@@ -4,21 +4,31 @@ using System.Collections;
 public class HandlePortalDeath : MonoBehaviour {
 
     private static bool isShuttingDown = false;
+    private static int numVotes = 0;
 
     public GUISkin skin;
+
+    void Start()
+    {
+        numVotes++;
+    }
  
     void OnApplicationQuit() {
         isShuttingDown = true;
     }
 
     void OnDestroy() {
-        if (isShuttingDown) return;
+        numVotes--;
+        if (numVotes == 0)
+        {
+            if (isShuttingDown) return;
 
-        Time.timeScale = 0;
-        Screen.lockCursor = false;
-        GameObject gameOver = new GameObject();
-        HandleGameOver message = gameOver.AddComponent<HandleGameOver>();
-        message.skin = skin;
-        message.gameOverMessage = "You Win!";
+            Time.timeScale = 0;
+            Screen.lockCursor = false;
+            GameObject gameOver = new GameObject();
+            HandleGameOver message = gameOver.AddComponent<HandleGameOver>();
+            message.skin = skin;
+            message.gameOverMessage = "You Win!";
+        }
     }
 }
