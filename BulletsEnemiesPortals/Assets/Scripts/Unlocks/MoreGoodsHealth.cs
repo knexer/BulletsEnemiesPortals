cb2580f2df1d753d6aa90002140e9ab3e9cbb2bb
knexer <knexer@gmail.com>
2014-08-25 17:39:18 -0700

    Add and integrate more health unlock

---
+++ BulletsEnemiesPortals/Assets/Scripts/Unlocks/MoreGoodsHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoreGoodsHealth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] goods = GameObject.FindGameObjectsWithTag("Good");
+        foreach (GameObject good in goods)
+        {
+            TakeDamage hp = good.GetComponent<TakeDamage>();
+            hp.HP = hp.HP * 2;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ BulletsEnemiesPortals/Assets/Scripts/Unlocks/MorePortalHealth.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MorePortalHealth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] enemyPortals = GameObject.FindGameObjectsWithTag("EnemyPortal");
+        foreach(GameObject enemyPortal in enemyPortals){
+            TakeDamage hp = enemyPortal.GetComponent<TakeDamage>();
+            hp.HP = hp.HP * 2;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
